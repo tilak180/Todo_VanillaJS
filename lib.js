b8a1bf8eac4c07bd60657d27fb863f8785e09c29
tilak180 <tilakruparel180@gmail.com>
2019-05-29 14:23:46 +0530

    Complete Strikeout List For ToDo

---
+++ lib.js
@@ -0,0 +1,19 @@
+document.getElementById("demo").innerHTML = "Hello World!"; 
+
+function checkboxChange(e) {
+    var checkboxId = e.target.id; // first-checkbox
+    var valueId = checkboxId.split('-')[0] + '-value' // first-value
+    var isChecked = e.target.checked;
+    var element = document.getElementById(valueId);
+    if (isChecked) {
+        var str = element.innerText;
+        console.log('innerText', str);
+        console.log('innerHtml', str.strike())
+        element.innerHTML = str.strike()
+    } else {
+        var str = element.innerText;
+        element.innerHTML = str;
+    }
+} 
+
+
